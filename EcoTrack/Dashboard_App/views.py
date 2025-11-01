23c1a09961a88336fa134ccc4ab5aa1d8f038783
sittiemacasindeL <sittiesharima.macasindel@cit.edu>
2025-11-01 17:54:02 +0800

    New complete Dashboard

--- EcoTrack/Dashboard_App/views.py
+++ EcoTrack/Dashboard_App/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def dashboard(request):
+    """
+    Dashboard view for EcoTrack application
+    """
+    context = {
+        'user': request.user,
+        # Add any other context data you need
+    }
+    return render(request, 'Dashboard_App/dashboard.html', context)
