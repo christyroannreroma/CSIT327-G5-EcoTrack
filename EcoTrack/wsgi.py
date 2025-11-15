6595aaabaabe36d113e0269266dd6f9dab7e4cef
sanggreterra <joji.matsuda@cit.edu>
2025-11-15 19:09:20 +0800

    Add login and signup functionality with templates and styles
    - Created login and signup HTML templates with integrated styles.
    - Implemented views for user signup and login, including validation and error handling.
    - Added URL routing for login and signup views.
    - Developed client-side validation for signup form using JavaScript.
    - Established a basic test structure for both Login and Signup apps.
    - Integrated success and error messaging for user feedback during authentication processes.
    - Created CSS styles for consistent UI across login and signup pages.

--- EcoTrack/wsgi.py
+++ EcoTrack/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EcoTrack.EcoTrack.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EcoTrack.settings')
 
 application = get_wsgi_application()
